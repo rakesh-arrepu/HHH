a478efc89d48601cd58a0748f3543612e6739416
rakesh-arrepu <38352324+rakesh-arrepu@users.noreply.github.com>
2025-10-09 22:53:13 +0530

    chore: add ESLint flat config for CI/pre-commit

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,4 +1,17 @@
-module.exports = {
-  // Load configuration from legacy .eslintrc.json
-  extends: ['./.eslintrc.json'],
-};
+const react = require('eslint-plugin-react');
+
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    plugins: { react },
+    rules: {},
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+];
